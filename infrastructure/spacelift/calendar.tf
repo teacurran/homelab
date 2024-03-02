569bd41151952bdf85249a204788846ba0bc7659
Terrence Curran <tcurran@approachingpi.com>
2024-03-02 14:07:16 -0500

    spacelift config

--- infrastructure/spacelift/calendar.tf
+++ infrastructure/spacelift/calendar.tf
@@ -3,3 +3,14 @@ resource "spacelift_space" "calendar" {
   parent_space_id = spacelift_space.mostly_Cats.id
   inherit_entities = true
 }
+
+resource "spacelift_stack" "calendar-production" {
+  name                     = "Calendar - production"
+  space_id                 = spacelift_space.calendar.id
+  repository               = "design"
+  branch                   = "main"
+  project_root             = "calendar/infrastructure"
+  manage_state             = false
+  workspace               = "production"
+  terraform_version        = "1.5.7 - 1.6.2"
+}
