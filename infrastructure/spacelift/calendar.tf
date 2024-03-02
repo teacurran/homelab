6bd00e76357d45a891331367c17133e897274e6c
Terrence Curran <tcurran@approachingpi.com>
2024-03-02 14:43:56 -0500

    commented out aws integration for now

--- infrastructure/spacelift/calendar.tf
+++ infrastructure/spacelift/calendar.tf
@@ -15,7 +15,7 @@ resource "spacelift_stack" "calendar-production" {
   terraform_version        = "1.5.7 - 1.6.2"
 }
 
-resource "spacelift_aws_integration_attachment" "calendar-production" {
-  stack_id = spacelift_stack.calendar-production.id
-  integration_id = data.spacelift_aws_integration.default.id
-}
+# resource "spacelift_aws_integration_attachment" "calendar-production" {
+#   stack_id = spacelift_stack.calendar-production.id
+#   integration_id = data.spacelift_aws_integration.default.id
+# }
