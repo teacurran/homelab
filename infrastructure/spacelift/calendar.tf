08c21f06ff22066d2bc97fc8b02f19beb2ec7ec4
Terrence Curran <tcurran@approachingpi.com>
2024-03-02 14:25:48 -0500

    typo

--- infrastructure/spacelift/calendar.tf
+++ infrastructure/spacelift/calendar.tf
@@ -1,6 +1,6 @@
 resource "spacelift_space" "calendar" {
   name = "Calendar"
-  parent_space_id = spacelift_space.mostly_Cats.id
+  parent_space_id = spacelift_space.mostly_cats.id
   inherit_entities = true
 }
 
