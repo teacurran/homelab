2ab180319e92b4be5aa2a822a7828cfe496ec934
Terrence Curran <tcurran@approachingpi.com>
2024-03-02 15:54:59 -0500

    terraform

--- infrastructure/spacelift/spacelift.tf
+++ infrastructure/spacelift/spacelift.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_version = "~> 1.5.7"
+
+  required_providers {
+    aws = {
+      version = "~> 5.30.0"
+      source  = "hashicorp/aws"
+    }
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
+  }
+
+  backend "s3" {
+    bucket = "appi-infrastructure-terraform-state"
+    key    = "infrastructure-spacelift.tfstate"
+    region = "us-east-1"
+  }
+}
+
 variable "spacelift_key_id" {}
 variable "spacelift_key_secret" {}
 
@@ -5,14 +25,6 @@ provider "spacelift" {
   api_key_endpoint = "https://teacurran.app.spacelift.io"
   api_key_id       = var.spacelift_key_id
   api_key_secret   = var.spacelift_key_secret
-}
-
-terraform {
-  required_providers {
-    spacelift = {
-      source = "spacelift-io/spacelift"
-    }
-  }
 }
 
 data "spacelift_space_by_path" "base" {
